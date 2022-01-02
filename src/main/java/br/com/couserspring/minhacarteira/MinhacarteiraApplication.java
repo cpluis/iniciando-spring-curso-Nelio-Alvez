5a5efa0f0c54cf6ed2a85a41240d0bdb85028d01
luis.pires <luispires.m@gmail.com>
2022-01-01 22:35:39 -0300

    Dando novos passos com java e Spring

---
+++ src/main/java/br/com/couserspring/minhacarteira/MinhacarteiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.couserspring.minhacarteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhacarteiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhacarteiraApplication.class, args);
+	}
+
+}
