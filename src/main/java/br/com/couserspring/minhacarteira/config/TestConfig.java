bd2bb0bdafb533b0d723b20e5ac0314e29238e9c
luis.pires <luispires.m@gmail.com>
2022-01-03 23:03:49 -0300

    OrderStatus enum

--- src/main/java/br/com/couserspring/minhacarteira/config/TestConfig.java
+++ src/main/java/br/com/couserspring/minhacarteira/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.couserspring.minhacarteira.entities.Order;
 import br.com.couserspring.minhacarteira.entities.User;
+import br.com.couserspring.minhacarteira.entities.enums.OrderStatus;
 import br.com.couserspring.minhacarteira.repositories.OrderRepository;
 import br.com.couserspring.minhacarteira.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Pablo Pereira", "paulopereira@gmail.com", "977777777", "123456"); 
 
 		// neste momento está ocorrendo a assiciação 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1); 
 
 	
 		userRepository.saveAll(Arrays.asList(u1,u2));
