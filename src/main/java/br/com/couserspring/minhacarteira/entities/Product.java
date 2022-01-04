d7f866090d23b50c9114bb3907e8f6d6a9108af8
luis.pires <luispires.m@gmail.com>
2022-01-04 01:09:08 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/couserspring/minhacarteira/entities/Product.java
+++ src/main/java/br/com/couserspring/minhacarteira/entities/Product.java
@@ -9,9 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
 
 @Entity
 @Table(name = "tb_product")
@@ -25,15 +26,16 @@ public class Product implements Serializable {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	@Transient
-	private Set<Category> categories = new HashSet<>();
-	
-	
-	public Product() {
-		
-	}
 
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+
+	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
@@ -43,46 +45,37 @@ public class Product implements Serializable {
 		this.imgUrl = imgUrl;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public Double getPrice() {
 		return price;
 	}
 
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
 
 	public String getImgUrl() {
 		return imgUrl;
@@ -96,12 +89,10 @@ public class Product implements Serializable {
 		return categories;
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -114,6 +105,5 @@ public class Product implements Serializable {
 		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }
